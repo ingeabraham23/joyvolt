1c05a96de5d59c044d97d53a13f3cd697b866320
ingeabraham23 <ingeabraham23@gmail.com>
2025-07-17 19:19:38 -0500

    gif de feria

--- src/components/GifGallery.jsx
+++ src/components/GifGallery.jsx
@@ -3,17 +3,38 @@ import React from "react";
 import "./GifGallery.css";
 
 const gifList = [
+  "corazon.gif",
+  "cranky_01.gif",
+  "cranky_02.gif",
   "donkey.gif",
+  "feria.gif",
+  "feria_1.gif",
+  "feria_1_ultrahd.gif",
+  "feria_2.gif",
+  "feria_2_ultrahd.gif",
+  "ghost_pacmanonda.gif",
   "honda.gif",
   "ken.gif",
   "kong_music.gif",
   "mario_bros.gif",
+  "mario_chiquito.gif",
+  "mario_long.gif",
   "mario_turtle.gif",
   "marios.gif",
+  "matrix_codigo.gif",
+  "matrix_joyboy.gif",
+  "matrix_reloaded.gif",
+  "matrix_reloaded_ultra.gif",
+  "onda_cabeza.gif",
   "pacman.gif",
+  "pacman_2.gif",
+  "pacman_neon.gif",
   "ryu.gif",
   "ryu_cammy.gif",
+  "status.gif",
+  "status_b.gif",
   "yoshi.gif",
+  "you_onda.gif",
 
   // Agrega aquí todos los nombres de tus GIFs
 ];
